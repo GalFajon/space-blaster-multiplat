01aa63c34ebdc656bda39f95d4e0343fe0539ef8
GalFajon <gla.nojaf@gmail.com>
2025-01-28 23:38:21 +0100

    Fullscreen patch.

--- GameEngine/Game.cs
+++ GameEngine/Game.cs
@@ -40,7 +40,7 @@ namespace GameEngine
             graphics.PreferredBackBufferWidth = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Width;
             graphics.PreferredBackBufferHeight = GraphicsAdapter.DefaultAdapter.CurrentDisplayMode.Height;
 
-            //graphics.IsFullScreen = true;
+            graphics.IsFullScreen = true;
             graphics.SynchronizeWithVerticalRetrace = false;
             graphics.ApplyChanges();
 
